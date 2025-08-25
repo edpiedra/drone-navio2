2c5dac15f808761488a5f971fbbf00c49d5ea867
edpiedra <edpiedra@mac.com>
2025-08-25 16:52:53 -0700

    update filesystem exansion

--- install/scripts/10_system_packages.sh
+++ install/scripts/10_system_packages.sh
@@ -5,11 +5,19 @@ SCRIPT_NAME=$(basename "$0")
 source "$MAIN_SCRIPTS_DIR/00_common.env"
 source "$MAIN_SCRIPTS_DIR/00_lib.sh"
 
+INSTALL_FLAG="$LOG_DIR/filesystem-expansion"
+
 log "updating system packages..."
 sudo apt-get update && sudo apt-get -y -qq dist-upgrade
 
-log "exanding filesystem..."
-sudo raspi-config --expand-rootfs
+if [ ! -f "$INSTALL_FLAG"]; then
+    log "exanding filesystem..."
+    sudo raspi-config --expand-rootfs
+    touch "$INSTALL_FLAG"
+fi 
 
 log "preparing for reboot..."
 sudo bash "$MAIN_SCRIPTS_DIR/11_system_reboot.sh"
+
+read -p "→ Exanded filesystem. Press ENTER to reboot." _
+sudo reboot
