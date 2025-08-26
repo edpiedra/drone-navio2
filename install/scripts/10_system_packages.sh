0f5877ef0099354f22c2cd9515591b1e5cd44ce5
edpiedra <edpiedra@mac.com>
2025-08-25 17:18:55 -0700

    fixing unbound variable

--- install/scripts/10_system_packages.sh
+++ install/scripts/10_system_packages.sh
@@ -10,13 +10,14 @@ INSTALL_FLAG="$LOG_DIR/filesystem-expansion"
 log "updating system packages..."
 sudo apt-get update && sudo apt-get -y -qq dist-upgrade
 
-if [ ! -f "$INSTALL_FLAG"]; then
+if [ ! -f "$INSTALL_FLAG" ]; then
     log "exanding filesystem..."
     sudo raspi-config --expand-rootfs
     touch "$INSTALL_FLAG"
 fi 
 
 log "preparing for reboot..."
+export MAIN_SCRIPTS_DIR=$MAIN_SCRIPTS_DIR
 sudo bash "$MAIN_SCRIPTS_DIR/11_system_reboot.sh"
 
 read -p "→ Exanded filesystem. Press ENTER to reboot." _
